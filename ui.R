618571b7e2d58d13efbe8ea45b7c262e72a813ce
David Shanabrook <davidshanabrook@me.com>
2015-09-03 20:23:50 -0400

    submitButton
    I think we need this

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ h3("Option Open Interest"),
     plotOutput("OIplot"),
 	column(4,
 	textInput("ticker","Ticker", value="AAPL")
-#	,submitButton(text = "Go", icon = NULL)
+	,submitButton(text = "Go", icon = NULL)
 	),
 	column(4,
 	checkboxInput("allStrikes","Use all strikes?", value=FALSE),
